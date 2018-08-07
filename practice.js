a45038914ae2fea33d294afff4d14dec56d0b5c4
Hunter Carrico <40701152+hmcarrico@users.noreply.github.com>
2018-08-07 16:47:54 -0700

    Update practice.js

--- practice.js
+++ practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr) {
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +36,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr) {
+  return arr[arr.length -1]
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family) {
+  for(i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -64,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters){
+  for(let i = letters.length -1; i >=0; i--){
+    alert(letters[i])
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -79,20 +91,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
-
-
-
+function evenFinder(nums){
+  for(let i = nums.length -1; i >=0; i--){
+   if(nums[i] % 2 === 1) {
+    nums.splice(i,1);
+   }
+  }
+  return nums;
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
-
-
-
-
-
-
 
 ////////// PROBLEM 6 //////////
 
@@ -107,8 +116,19 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 //Code Here
 
-
-
+function divider(numbersArray){
+  
+  let evens = [];
+  let odds = [];
+  for(i = 0; i < numbersArray.length; i++) {
+    if(numbersArray[i] % 2 === 0){
+      evens.push(numbersArray[i]);
+    } else{
+      odds.push(numbersArray[i]);
+    } 
+  }
+  return [evens, odds];
+}
 ////////// PROBLEM 7 //////////
 
 // Do not edit the code below.
